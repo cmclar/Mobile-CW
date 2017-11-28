056ee1f752ccf6f6e183c2f29ad808220a49557f
discostyles <cmclar204@caledonian.ac.uk>
2017-11-28 22:52:35 +0000

    controls and background added

--- app/src/main/assets/PhoneMario.js
+++ app/src/main/assets/PhoneMario.js
@@ -28,19 +28,33 @@ class aSprite {
   }
 
   // Method
-  render()
+  render(width, height)
   {
-    canvasContext.drawImage(this.sImage,this.x, this.y);
+    canvasContext.drawImage(this.sImage,this.x, this.y, width, height);
   }
   // Method
-  scrollBK(delta)
+  scrollBK(delta, width, height)
   {
     //var xPos = delta * this.vx;
 
     canvasContext.save();
     canvasContext.translate(-delta, 0);
-    canvasContext.drawImage(this.sImage,0, 0);
-    canvasContext.drawImage(this.sImage,this.sImage.width, 0);
+    canvasContext.drawImage(this.sImage,0, 0, width, height);
+    canvasContext.drawImage(this.sImage,this.sImage.width, 0, width, height);
+    canvasContext.restore();
+  }
+
+  scrollBrick(delta, width, height)
+  {
+    //var xPos = delta * this.vx;
+
+    canvasContext.save();
+    canvasContext.translate(-delta, 0);
+    canvasContext.drawImage(this.sImage, 0, canvas.height - height, width, height);
+    canvasContext.drawImage(this.sImage, width, canvas.height - height, width, height);
+    canvasContext.drawImage(this.sImage, 2 * width, canvas.height - height, width, height);
+    canvasContext.drawImage(this.sImage, 3 * width, canvas.height - height, width, height);
+    canvasContext.drawImage(this.sImage, 4 * width, canvas.height - height, width, height);
     canvasContext.restore();
   }
   // Method
@@ -80,6 +94,9 @@ var leftIcon;
 var rightIcon;
 var lastPt=null;
 var gameOverScreen = false;
+var elapsed = null;
+var leftPressed = false;
+var rightPressed = false;
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -89,12 +106,14 @@ function resizeCanvas() {
 function resizeCar() {
   theCar.sImage.width = window.innerWidth/5;
   theCar.sImage.height = window.innerHeight/5;
-  console.log(theCar.sImage.height*5);
+  //console.log(theCar.sImage.height*5);
 }
 
 function resizeBackground() {
-  bkgdImage.sImage.height = window.innerHeight;
-  console.log(bkgdImage.sImage.height);
+  //console.log(canvas.height)
+  //console.log(bkgdImage.sImage.height)
+  bkgdImage.sImage.height = canvas.height;
+  //console.log(bkgdImage.sImage.height);
 }
 
 function load()
@@ -109,7 +128,7 @@ function init() {
   if (canvas.getContext) {
     //Set Event Listeners for window, mouse and touch
     window.addEventListener('resize', resizeCanvas, false);
-    window.addEventListener('orientationchange', resizeCanvas, true);
+    window.addEventListener('orientationchange', resizeCanvas, false);
 
     canvas.addEventListener("touchstart", touchDown, false);
     canvas.addEventListener("touchmove", touchXY, true);
@@ -119,56 +138,68 @@ function init() {
 
     resizeCanvas();
 
-    bkgdImage = new aSprite(0,0,"Road.png", 100, 0, "Generic");
-    theCar = new aSprite(100,0,"car.png", 0, 0, "Generic");
+    bkgdImage = new aSprite(0,0,"clouds.png", 100, 0, "Generic");
+    theCar = new aSprite(100,0,"mario.png", 0, 0, "Generic");
     left = new aSprite(100, 0, "left.png", 0, 0, "Generic");
+    right = new aSprite(100, 0, "right.png", 0, 0, "Generic");
+    brick1 = new aSprite(100, 0, "brick.png", 100, 0, "Generic");
+//    brick2 = new aSprite(100, 0, "brick.png", 100, 0, "Generic");
+//    brick3 = new aSprite(100, 0, "brick.png", 100, 0, "Generic");
 
-    resizeBackground();
-    resizeCar();
+    console.log(bkgdImage.sImage.height);
+    console.log(theCar.sImage.height);
+    console.log(left.sImage.height);
 
-    theCar.sPos(window.innerWidth/10,3*window.innerHeight/5);
-    left.sPos(window.innerWidth/10,4*window.innerHeight/5);
-    //console.log(theCar.y);
+    theCar.sPos(canvas.width/10, canvas.height - (3*canvas.height/20 + window.innerHeight/15));
+    left.sPos(canvas.width/10, 9 * canvas.height/10);
+    right.sPos(3*canvas.width/10, 9 * canvas.height/10);
     startTimeMS = Date.now();
     gameLoop();
   }
 }
 
 function gameLoop(){
-  //console.log("gameLoop");
-  var elapsed = (Date.now() - startTimeMS)/1000;
-  travel += elapsed * bkgdImage.vx;
+  elapsed = (Date.now() - startTimeMS)/1000;
+  if (leftPressed)
+  {
+    travel -= elapsed * bkgdImage.vx;
+  }
+  if (rightPressed)
+  {
+    travel += elapsed * bkgdImage.vx;
+  }
+
   if (travel > bkgdImage.sImage.width)
   {
     travel = 0;
   }
-
   update(elapsed);
   render(elapsed);
+
   startTimeMS = Date.now();
   requestAnimationFrame(gameLoop);
 }
 
 function render(delta) {
   canvasContext.clearRect(0,0,canvas.width, canvas.height);
-  bkgdImage.scrollBK(travel);
-  theCar.render();
-  left.render();
+  bkgdImage.scrollBK(travel, bkgdImage.sImage.width, canvas.height);
+  brick1.scrollBrick(travel, brick1.sImage.width/3, 3*canvas.height/20);
+  theCar.render(window.innerWidth/15, window.innerHeight/15);
+  left.render(window.innerWidth/10, window.innerHeight/10);
+  right.render(window.innerWidth/10, window.innerHeight/10);
 }
 
 function update(delta) {
 }
 
 function collisionDetection() {
-  console.log("x");
-  console.log(left.x);
-  console.log(lastPt.x);
-  console.log("y");
-  console.log(left.y);
-  console.log(lastPt.x);
-  if ((lastPt.x > left.x && lastPt.x < left.sImage.width) && (lastPt.y > left.y && lastPt.y < left.sImage.height))
+  if ((lastPt.x > left.x && lastPt.x < (left.x + left.sImage.width)) && (lastPt.y > left.y && lastPt.y < (left.y + left.sImage.height)))
   {
-    travel += elapsed * bkgdImage.vx;
+    leftPressed = true;
+  }
+  if ((lastPt.x > right.x && lastPt.x < (right.x + right.sImage.width)) && (lastPt.y > right.y && lastPt.y < (right.y + right.sImage.height)))
+  {
+    rightPressed = true;
   }
 }
 
@@ -184,23 +215,25 @@ function touchUp(evt) {
   evt.preventDefault();
   // Terminate touch path
   lastPt=null;
+  leftPressed = false;
+  rightPressed = false;
 }
 
 function touchDown(evt) {
   evt.preventDefault();
-  if(gameOverScreen)
-  {
-    return;
-  }
-  touchXY(evt);
-}
-
-function touchXY(evt) {
-  evt.preventDefault();
+  //if(gameOverScreen)
+  //{
+  //  return;
+  //}
+  //touchXY(evt);
   if(lastPt!=null) {
     var touchX = evt.touches[0].pageX - canvas.offsetLeft;
     var touchY = evt.touches[0].pageY - canvas.offsetTop;
   }
   lastPt = {x:evt.touches[0].pageX, y:evt.touches[0].pageY};
   collisionDetection(evt);
+}
+
+function touchXY(evt) {
+  evt.preventDefault();
 }
